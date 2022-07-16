90efbc6c84107c57787ebcf6bbd6f2f2d535bb19
Marcelo Bogado <celombc@gmail.com>
2022-07-15 22:31:10 -0300

    first component + other components + Datepicker + useState

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import logo from '../../assets/img/logo.svg'
+
+function Header() {
+    return (
+        <header>
+            <div className="container logo">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a></p>
+            </div>
+        </header>
+    )
+}
+export default Header
